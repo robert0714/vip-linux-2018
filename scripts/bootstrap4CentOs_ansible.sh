4a00994af436ee02abe02738b6d7a37ffc558091
Robert <robert0714@gmail.com>
2017-02-26 07:09:14 +0800

    Update bootstrap4CentOs_ansible.sh

--- scripts/bootstrap4CentOs_ansible.sh
+++ scripts/bootstrap4CentOs_ansible.sh
@@ -19,9 +19,9 @@ if [[ $testing != ""  ]]; then
 else
       echo "Ansible is not installed."
       yum install -y wget
-      wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-      rpm -ivh  epel-release-7-8.noarch.rpm
-      rm -rf epel-release-7-8.noarch.rpm
+      wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+      rpm -ivh  epel-release-7-9.noarch.rpm
+      rm -rf epel-release-7-9.noarch.rpm
       
 fi
 
