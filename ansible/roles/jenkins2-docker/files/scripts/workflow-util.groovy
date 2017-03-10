6db1e3809900dc11d0c44160d12f44871c8bb664
robert.lee <robert0714@gmail.com>
2017-03-10 16:53:34 +0800

    add projects

--- ansible/roles/jenkins2-docker/files/scripts/workflow-util.groovy
+++ ansible/roles/jenkins2-docker/files/scripts/workflow-util.groovy
@@ -1,3 +1,137 @@
+import groovy.json.JsonSlurper
+import org.apache.commons.httpclient.HttpClient
+import org.apache.commons.httpclient.methods.GetMethod
+
+
+def provision(playbook) {
+    stage "Provision"
+    env.PYTHONUNBUFFERED = 1
+    sh "ansible-playbook /vagrant/ansible/${playbook} \
+        -i /vagrant/ansible/hosts/prod"
+}
+
+def buildTests(serviceName, registryIpPort) {
+    stage "Build tests"
+    def tests = docker.image("${registryIpPort}/${serviceName}-tests")
+    println "image: ${registryIpPort}/${serviceName}-tests"
+    try {
+        tests.pull()
+    } catch(e) {
+       println "${e}"
+    }
+    sh "docker build -t \"${registryIpPort}/${serviceName}-tests\" \
+        -f Dockerfile.test ."
+    tests.push()
+}
+
+def runTests(serviceName, target, extraArgs) {
+    stage "Run ${target} tests"
+    println "serviceName: ${serviceName}"
+    println "target: ${target}"
+    println "extraArgs: ${extraArgs}"
+    sh "COMPOSE_HTTP_TIMEOUT=200   docker-compose -f docker-compose-dev.yml \
+        run --rm ${extraArgs} ${target}"
+}
+
+def buildService(serviceName, registryIpPort) {
+    stage "Build service"
+    def service = docker.image("${registryIpPort}/${serviceName}")
+    try {
+        service.pull()
+    } catch(e) {}
+    docker.build "${registryIpPort}/${serviceName}"
+    service.push()
+}
+
+def deploy(serviceName, prodIp) {
+    stage "Deploy"
+    withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+        sh "echo   $DOCKER_HOST"
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose pull app"
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose -p ${serviceName} up -d app"
+    }
+}
+
+def deployBG(serviceName, prodIp, color) {
+    stage "Deploy"
+    withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose pull app-${color}"
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose -p ${serviceName} up -d app-${color}"
+    }
+}
+
+def deploySwarm(serviceName, swarmIp, color, instances) {
+    stage "Deploy"
+    withEnv(["DOCKER_HOST=tcp://${swarmIp}:2375"]) {
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose -f docker-compose-swarm.yml \
+            pull app-${color}"
+        try {
+            sh "docker network create ${serviceName}  --driver overlay"
+        } catch (e) {}
+        sh "COMPOSE_HTTP_TIMEOUT=200  docker-compose -f docker-compose-swarm.yml \
+            -p ${serviceName} up -d db"
+        sh "COMPOSE_HTTP_TIMEOUT=200 docker-compose -f docker-compose-swarm.yml \
+            -p ${serviceName} rm -f app-${color}"
+        sh "COMPOSE_HTTP_TIMEOUT=200  docker-compose -f docker-compose-swarm.yml \
+            -p ${serviceName} scale app-${color}=${instances}"
+    }
+    putInstances(serviceName, swarmIp, instances)
+}
+
+def updateProxy(serviceName, proxyNode) {
+    stage "Update proxy"
+    stash includes: 'nginx-*', name: 'nginx'
+    node(proxyNode) {
+        unstash 'nginx'
+        sh "sudo cp nginx-includes.conf /data/nginx/includes/${serviceName}.conf"     
+        sh "sudo /usr/local/bin/consul-template \
+            -consul localhost:8500 \
+            -template \"nginx-upstreams.ctmpl:/data/nginx/upstreams/${serviceName}.conf:docker kill -s HUP nginx\" \
+            -once"
+    }
+}
+
+def runBGPreIntegrationTests(serviceName, prodIp, color) {
+    stage "Run pre-integration tests"
+    def address = getAddress(serviceName, prodIp, color)
+    try {
+        runTests(serviceName, "integ", "-e DOMAIN=http://${address}")
+    } catch(e) {
+        stopBG(serviceName, prodIp, color);
+        error("Pre-integration tests failed")
+        println "error details: ${e}"
+    }
+}
+
+def runBGPostIntegrationTests(serviceName, prodIp, proxyIp, proxyNode, currentColor, nextColor) {
+    stage "Run post-integration tests"
+    try {
+        runTests(serviceName, "integ", "-e DOMAIN=http://${proxyIp}")
+    } catch(e) {
+        if (currentColor != "") {
+            updateBGProxy(serviceName, proxyNode, currentColor)
+        }
+        stopBG(serviceName, prodIp, nextColor);
+        error("Post-integration tests failed")
+        println "error details: ${e}"
+    }
+    stopBG(serviceName, prodIp, currentColor);
+}
+
+def stopBG(serviceName, prodIp, color) {
+    if (color.length() > 0) {
+        stage "Stop"
+        withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+            sh "docker-compose -p ${serviceName} stop app-${color}"
+        }
+    }
+}
+
+def updateBGProxy(serviceName, proxyNode, color) {
+    stage "Update proxy"
+    stash includes: 'nginx-*', name: 'nginx'
+    node(proxyNode) {
+        unstash 'nginx'
         sh "sudo cp nginx-includes.conf /data/nginx/includes/${serviceName}.conf"
         sh "sudo /usr/local/bin/consul-template \
             -consul localhost:8500 \
